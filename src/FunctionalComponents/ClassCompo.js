14e6a24b1110de9b5d32d66eae7e5813ef4723a2
Rahul Pahuja <rahulpahuja2015@gmail.com>
2023-10-17 11:38:48 +0530

    Added Ne Components

---
+++ src/FunctionalComponents/ClassCompo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+class ClassComponent extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>This is a class Component classComponent</h1>
+            </div>
+            );
+    }    
+}
+
+export default ClassComponent;
---
+++ src/FunctionalComponents/FunctionalComp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function FunctionalComponent(){
+    return(
+        <h1>This is a functional functionalComponent</h1>
+    );
+}
+
+export default FunctionalComponent;
